a9047d33b149f2a0f1516991a5143610a7371648
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-23 13:04:19 -0600

    multiply

---
+++ multiply.php
@@ -0,0 +1,11 @@
+<?php
+// The code does not execute properly. Try to figure out why.
+// function multiply(a, b) {
+//   return a * b
+// }
+
+function multiply($a, $b) {
+  return $a * $b;
+}
+
+?>
