9d5f9d6a0851e85560cac63b0576ac5020d8d3b3
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-04-10 12:11:10 -0600

    reworked an old problem

--- divisors.php
+++ divisors.php
@@ -2,19 +2,10 @@
 // Create a function named that takes an integer and returns an array with all of the integer's divisors(except for 1 and the number itself). If the number is prime return the string '(integer) is prime'.
 
 function divisors($integer) {
-  // add divisors to an array
-  $intDivisors = [];
-  // loop over $integer variable
-  for ($i = 2; $i < $integer; $i++)
-  {
-  // find the $integer divisors
-    if (!($integer % $i))
-    {
-  // retrun divisors, except for 1 and the $integer itself
-      $intDivisors[] = $i;
-    }
-  }
-  // if prime print $integer . ' is prime'
-  return $intDivisors ? : $integer . ' is prime';
+  $divsr = [];
+  for($i = 2; $i < $integer; $i++)
+    if($integer % $i == 0)
+      $divsr[] = $i;
+  return $divsr != [] ? $divsr : $integer . ' is prime';
 }
  ?>
