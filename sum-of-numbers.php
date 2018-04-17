eb7888d04c6c2f0165e448f8fbb4ba20703d2420
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-04-17 09:21:58 -0600

    sum of numbers from 0 to N

--- sum-of-numbers.php
+++ sum-of-numbers.php
@@ -1,5 +1,21 @@
 <?php
 
+// generate a function that computes the series starting from 0 and ending until the given number following the sequence:
+
+// 0 1 3 6 10 15 21 28 36 45 55 ....
+
+// which is created by
+
+// 0, 0+1, 0+1+2, 0+1+2+3, 0+1+2+3+4, 0+1+2+3+4+5, 0+1+2+3+4+5+6, 0+1+2+3+4+5+6+7 etc..
+
+// Input:
+
+// LastNumber
+
+// Output:
+
+// series and result
+
 class SequenceSum {
   public function showSequence($count) {
      if ($count < 0) {
