9264fbf1a6b576dc357ff86cacea522b8c698994
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-05-01 08:36:29 -0600

    attempted divisors challange again.

--- divisors.php
+++ divisors.php
@@ -2,12 +2,13 @@
 // Create a function named that takes an integer and returns an array with all of the integer's divisors(except for 1 and the number itself). If the number is prime return the string '(integer) is prime'.
 
 function divisors($integer) {
-  $divsr = [];
-
-  for($i = 2; $i < $integer; $i++)
-    if($integer % $i == 0)
-      $divsr[] = $i;
+  $divArr = [];
       
-  return $divsr != [] ? $divsr : $integer . ' is prime';
+  for ($i = 2; $i < $integer; $i++) {
+    if ($integer % $i == 0) {
+      $divArr[] = $i;
+    }
+  }
+  
+  return $divArr != [] ? $divArr : $integer . ' is prime';
 }
- ?>
